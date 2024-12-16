c2d775702b84bcbc337f4e199a5217b73c1399e0
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-12-16 13:14:26 +0200

    create a few tests with mocha

---
+++ pugs-of-flickr/flickr-fetcher.js
@@ -0,0 +1,7 @@
+const FlickrFetcher = {
+     photoObjToURL: function() {
+        return 'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg';
+    }
+};
+
+export default FlickrFetcher;
